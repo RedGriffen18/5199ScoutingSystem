5f5188bb992f54d7e8f3a724ba1f7fe0bb244eb4
RedGriffen18 <101958532+RedGriffen18@users.noreply.github.com>
2023-02-13 13:28:31 -0800

    Incorrect Directory

--- StartServer.bat
+++ StartServer.bat
@@ -1,4 +1,4 @@
-set PATH=%PATH%;%CD%\xampp\php
+set PATH=%PATH%;%CD%\php
 for /f "tokens=2 delims=:" %%a in ('ipconfig ^| findstr IPv4') do set IP=%%a
 start php -S %IP%:5199
 start chrome http://%IP%:5199/
